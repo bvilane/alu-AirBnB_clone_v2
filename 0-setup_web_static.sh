1223a0aefd4f51520ada3be2bce55fb32aa92e9b
bvilane <b.vilane@alustudent.com>
2024-02-16 13:45:15 -0800

    fix

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,56 +1,13 @@
 #!/usr/bin/env bash
-<<<<<<< HEAD
-# Sets up your web servers for the deployment of web_static
-sudo apt-get -y update
+# web static development
 
-# Install nginx
-sudo apt-get -y install nginx
-sudo ufw allow 'Nginx HTTP'
-# Create folders
-sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/shared/
-
-# Create HMTL 
-sudo touch /data/web_static/releases/test/index.html
-echo "
-<html>
- <head>
-   <title>
-   The title of your page
-   </title>
- </head>
- 
- <body>
-   Your page content goes here.
- </body>
- </html>" > sudo /data/web_static/releases/test/index.html
-
-# Symbolic link
-sudo ln -sfn /data/web_static/releases/test/ /data/web_static/current
-
-# Create ownership
-sudo chown -R ubuntu:ubuntu /data/
-
-# Setup nginx
-sudo sed -i '/listen 80 default_server/a location /hbnb_static/ {alias /data/web_static/current/;}' /etc/nginx/sites-available/default
-
-#Restart nginx
-sudo service nginx restart
-=======
-# Install Nginx if not already installed
 sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y install nginx
-
-# create folders
 sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-#create a fake html file
-echo "This is a test" | sudo tee /data/web_static/releases/test/index.html
-# create symbolic link
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-# give ownership
-sudo chown -hR ubuntu:ubuntu /data/
-sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-sudo service nginx start
->>>>>>> 2ca41524e28f0bfa8bb00a60e6c5ef5b347fe8b6
+echo "Hello, this is a test HTML file." | sudo tee /data/web_static/releases/test/index.html
+sudo rm -rf /data/web_static/current
+sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i '44i \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
+sudo service nginx restart
